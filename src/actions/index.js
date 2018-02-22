013eb98de2e1c97d79821a48efbe1fb8ada12364
Lazar Nikolić <casadifianco@gmail.com>
2018-02-22 23:38:48 +0100

    cleaned master branch
    cleaned master branch

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const SEND_USERNAME = 'SEND_USERNAME';
-
-export function sendUserName(term){
-  return{
-    type: SEND_USERNAME,
-    payload: term
-  }
-}
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import TeacherLogin from './teacher_login.js'
-
-
-const rootReducer = combineReducers({
-  teacherLogin: TeacherLogin
-});
-
-export default rootReducer;
