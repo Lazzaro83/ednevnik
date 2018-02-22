b3046554ec640254618e5e11689b4877816699c9
Lazar Nikolić <casadifianco@gmail.com>
2018-02-22 20:22:32 +0100

    Add files via upload

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
-import Rebase from 're-base';
+import Rebase from "re-base";
 
 const base = Rebase.createClass({
   apiKey: "AIzaSyCVwgTZ1WD_zFB5I4ApEVaYYTvsc37GQPw",
   authDomain: "edutech-elektronski-dnev-4fde6.firebaseapp.com",
-  databaseURL: "https://edutech-elektronski-dnev-4fde6.firebaseio.com",
+  databaseURL: "https://edutech-elektronski-dnev-4fde6.firebaseio.com"
 });
 
 export default base;
